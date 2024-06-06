999c699692cf4be4f1b91fd747d0330b49c5f192
guilbert <guilbertmartin75@gmail.com>
2024-06-06 15:37:38 +0200

    change 40vh to 45 vh fot points and recompenses

--- components/Rules.js
+++ components/Rules.js
@@ -8,7 +8,7 @@ function Rules() {
     const [showGoldenCircle, setShowGoldenCircle] = React.useState(false);
 console.log(showGoldenCircle)
   return (
-    <div className='h-[40vh] w-screen mt-10 landscapes:h-[80vh]'>
+    <div className='h-[45vh] w-screen mt-10 landscapes:h-[80vh]'>
         <div className='bg-white h-2/5 pt-4 landscapes:h-1/2 landscapes:pt-6'> 
             <h2 className="font-['Cormorand_Garamond'] font-bold pl-3 pb-2">REGLES DU JEU</h2>
             <p className='text-xs pl-3 pb-2'>Chers Artisans, </p>
@@ -20,7 +20,7 @@ console.log(showGoldenCircle)
         </div>
         <div className='bg-[url(/main_background.webp)] h-full bg-cover bg-no-repeat flex flex-row items-center pb-5 mt-5 landscapes:mt-0'>
             <div className='w-3/5 pr-2 mt-5 pt-3'>
-                <h2 className="font-['Cormorand_Garamond'] font-bold text-white pl-3 pb-2">POINTS</h2>
+                <h2 className="font-['Cormorand_Garamond'] font-bold text-white pl-3 pb-2 mt-5">POINTS</h2>
                 <p className='text-white text-xs pl-3 pb-3'>
                     Vous <span className='text-white font-bold'>gagnez des points</span> en fonction de vos interactions
                     avec les stories et avec les autres artisans Lunétoile.
@@ -34,7 +34,7 @@ console.log(showGoldenCircle)
                     L'ensemble des <span className='text-white font-bold '>membres de la maison</span> avec
                     le plus haut score sera récompensé <br/>(détails à venir).
                 </p>
-                <p className='text-white text-xs pl-3 pb-2'>
+                <p className='text-white text-xs pl-3 pb-5'>
                     Tous les <span className='text-white font-bold'>participants impliqués</span> seront récompensés
                 </p>
             </div>
