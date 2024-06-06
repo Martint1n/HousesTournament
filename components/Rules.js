d5170d69977dc9a7bd60c3c3303c467313a6d7f1
guilbert <guilbertmartin75@gmail.com>
2024-06-06 15:28:45 +0200

    responsive Regle du jeu ok

--- components/Rules.js
+++ components/Rules.js
@@ -8,8 +8,8 @@ function Rules() {
     const [showGoldenCircle, setShowGoldenCircle] = React.useState(false);
 console.log(showGoldenCircle)
   return (
-    <div className='h-[40vh] w-screen landscapes:h-[75vh] mt-5'>
-        <div className='bg-white h-2/5 pt-4 mb-5 '> 
+    <div className='h-[40vh] w-screen mt-10 landscapes:h-[80vh]'>
+        <div className='bg-white h-2/5 pt-4 landscapes:h-1/2 landscapes:pt-6'> 
             <h2 className="font-['Cormorand_Garamond'] font-bold pl-3 pb-2">REGLES DU JEU</h2>
             <p className='text-xs pl-3 pb-2'>Chers Artisans, </p>
             <p className='text-xs pl-3 pb-3'>
@@ -18,7 +18,7 @@ console.log(showGoldenCircle)
                 <span className='font-bold'>Tournoi des maisons.</span>
             </p>
         </div>
-        <div className='bg-[url(/main_background.webp)] h-full bg-cover bg-no-repeat flex flex-row items-center pb-5'>
+        <div className='bg-[url(/main_background.webp)] h-full bg-cover bg-no-repeat flex flex-row items-center pb-5 mt-5 landscapes:mt-0'>
             <div className='w-3/5 pr-2 mt-5 pt-3'>
                 <h2 className="font-['Cormorand_Garamond'] font-bold text-white pl-3 pb-2">POINTS</h2>
                 <p className='text-white text-xs pl-3 pb-3'>
