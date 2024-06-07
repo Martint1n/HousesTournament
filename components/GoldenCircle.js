e7035ff220e52c3e4baf9d6b315c40e033cf80a2
guilbert <guilbertmartin75@gmail.com>
2024-06-07 18:36:30 +0200

    change cerclegolden font bold

--- components/GoldenCircle.js
+++ components/GoldenCircle.js
@@ -10,9 +10,10 @@ function GoldenCircle(props) {
             <h2 className='font-["Cormorant_Garamond"] font-semibold text-white pb-2 pt-5'>LE CERCLE DORÉ</h2>
             <p className='text-xs text-white pb-2'>Pour<span className='text-xs text-white font-bold'> nous reconnaître </span>
             et renforcer le
-            <span className='text-xs text-white font-bold'> sentiment d'appartenance ajoutons un cercle doré autour de nos photos de profil. </span>
+            <span className='text-xs text-white font-bold'> sentiment d'appartenance </span>
+            ajoutons un cercle doré autour de nos photos de profil.
             </p>
-            <p className='text-xs text-gold italic pb-2'>Pour rejoindre le Cercle Doré <span className='text-xs text-gold font-bold italic'>envoyez-moi votre photo de profil</span> en MP.</p>
+            <p className='text-xs text-gold italic pb-2'>Pour rejoindre le Cercle Doré <span className='text-xs text-gold font-bold italic'>envoyez-moi votre photo </span> de profil en MP.</p>
           </div>
           <div className='w-2/5 pt-5 flex flex-col justify-center items-center landscapes:pb-2 landscapes:pt-2'>
             <Image src='/UMB.PNG' width={60} height={60} /> 
@@ -20,7 +21,7 @@ function GoldenCircle(props) {
               <Image src='/Fleche.svg' width={50} height={50} /> 
             </div>
             <Image src='/UMBD.PNG' width={60} height={60} />
-            <p className='text-white text-[8px] pt-1'>«Le cercle d'or c'est fantastique.»</p>
+            <p className='text-white text-[8px] pt-1'>«Le cercle doré c'est fantastique.»</p>
             <p className='text-white text-[10px] pb-10'>@uneminibiche</p>
           </div>
             </>
